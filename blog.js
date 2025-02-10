732f1edbe0b5f9a2d4a0d8942be49788307877ec
diegolenci <lencidiego0@gmail.com>
2025-02-10 16:17:53 +0100

    importazione di express

--- blog.js
+++ blog.js
@@ -1,2 +1,13 @@
 // 1: inizializzare npm init nel terminale
 // 2: installare express
+// 3: creazione gitignore
+// 4: importazione di express
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//attivazione del server
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
